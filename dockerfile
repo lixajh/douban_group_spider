7962b22b18e776c6d69bef3c66eb8cf6e437ee8e
xiaoli <lixajh@163.com>
2019-03-06 00:50:44 +0800

    修改配置

--- dockerfile
+++ dockerfile
@@ -2,6 +2,7 @@ FROM python:3.6.4
 
 RUN mkdir /code
 COPY . /code
+RUN rm /code/data/*
 RUN pip install -r /code/requirements.txt -i https://pypi.douban.com/simple
 WORKDIR /code
 
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-echo run.sh
-python3 ./service/web.py --config config.ini & python3 ./service/spider.py --config config.ini
+echo run start
+python3 ./service/web.py --config config.ini > web.log
+& python3 ./service/spider.py --config config.ini > spider.log
